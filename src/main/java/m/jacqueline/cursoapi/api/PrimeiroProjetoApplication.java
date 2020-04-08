876c52df10116e670a826bbc73db74b45874df32
Jacqueline Marinho <jacque_marinho@hotmail.com>
2020-04-08 13:39:02 -0300

    Versão inicial do projeto

---
+++ src/main/java/m/jacqueline/cursoapi/api/PrimeiroProjetoApplication.java
@@ -0,0 +1,13 @@
+package m.jacqueline.cursoapi.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/m/jacqueline/cursoapi/api/PrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package m.jacqueline.cursoapi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
